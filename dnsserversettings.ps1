84da7ac938ce7f1e5fadd3686869029baa7c2ce8
devsheoran <dev.sheoran@gmail.com>
2020-07-23 02:59:49 +0100

    Create dnsserversettings.ps1

---
+++ dnsserversettings.ps1
@@ -0,0 +1,3 @@
+Install-WindowsFeature DNS -IncludeManagementTools
+Add-DnsServerPrimaryZone -Name "uk.ntc.com" -ZoneFile "uk.ntc.com.dns"
+Add-DnsServerResourceRecordA -Name 'vm-spoke-01' -ZoneName 'uk.ntc.com' -IPv4Address 10.1.0.7
