f60f496e85228126f3f5a7ca69fa494affd37dd4
devsheoran <dev.sheoran@gmail.com>
2020-07-27 11:20:25 +0100

    Update deploy.ps1

--- deploy.ps1
+++ deploy.ps1
@@ -1,6 +1,6 @@
 param ($rgName, $location, $dnsZone, $vmAdminUserName, $vmAdminPwd,$deployVnet)
 
-New-AzureRmResourceGroup -Name '$rgName' -Location $location
+$rg = New-AzureRmResourceGroup -Name $rgName -Location $location
 
 $templateUri = "https://raw.githubusercontent.com/devsheoran/scripts/master/azuredeploy.simplehubspoketopology.json"
 $dnsIdentifier = $dnsZone
@@ -15,10 +15,10 @@ $parameters.Add("subscriptionId","$subscriptionID")
 $parameters.Add("dnsIdentifier",$dnsIdentifier)
 $parameters.Add("deployVnet",$deployVnet)
 
-Test-AzureRmResourceGroupDeployment -ResourceGroupName $rgName  -TemplateFile $templateUri -TemplateParameterObject $parameters -Verbose
+Test-AzureRmResourceGroupDeployment -ResourceGroupName $rg.Name  -TemplateFile $templateUri -TemplateParameterObject $parameters -Verbose
 
 #Set DNS Server - Forward lookup
 $Script = Invoke-WebRequest 'https://raw.githubusercontent.com/devsheoran/scripts/master/dnsserversettings.ps1'
 $ScriptBlock = [Scriptblock]::Create($Script.Content)
-Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList ($args + @($rgName,$dnsIdentifier,$vmAdminUserName,$vmAdminPassword))
+Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList ($args + @($rg.Name,$dnsIdentifier,$vmAdminUserName,$vmAdminPassword))
 
