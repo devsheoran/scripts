e5214cb46287f3e3dbbfd67997b76385f58e4f3a
devsheoran <dev.sheoran@gmail.com>
2020-07-27 10:40:50 +0100

    Update dnsserversettings.ps1

--- dnsserversettings.ps1
+++ dnsserversettings.ps1
@@ -1,3 +1,42 @@
-Install-WindowsFeature DNS -IncludeManagementTools
-Add-DnsServerPrimaryZone -Name "uk.ntc.com" -ZoneFile "uk.ntc.com.dns"
-Add-DnsServerResourceRecordA -Name 'vm-spoke-01' -ZoneName 'uk.ntc.com' -IPv4Address 10.1.0.4
+param ($rgName, $dnsZone)
+
+$Vms = Get-AzureRmVM -ResourceGroupName $rgName
+
+$templateUri = "C:\repo\ntc\ntc.infra\ntc.infra\ARM-Templates\azuredeploy.customextension.json"
+
+
+$vmAdminUserName ='adminuser'
+$vmAdminPassword=(ConvertTo-SecureString "P1sswor4@2020" -AsPlainText -Force)
+$subscriptionID = (Get-AzureRMContext).Subscription.id
+$extensionScriptHub ="powershell.exe"
+
+foreach($vm in $Vms)
+{
+$nic = Get-AzureRmNetworkInterface -ResourceGroupName $rgName -Name  ($vm.Name + "-nic") 
+  if($vm.Name -eq 'vm-ad-01')
+  {
+    $vmSubnetIdHub=$nic.IpConfigurations[0].Subnet.Id
+    $vmStaticIPHub=$nic.IpConfigurations[0].PrivateIpAddress
+    $vmNameHub=$vm.Name 
+    $vmSizeHub=$vm.HardwareProfile.VmSize
+    
+  }
+  if ($vm.Name -like '*-spoke-*')
+  {
+    $vm.Name 
+    $extensionScriptHub +=" Add-DnsServerResourceRecordA -Name " + $vm.Name  +" -ZoneName " + $dnsZone +" -IPv4Address " + $nic.IpConfigurations[0].PrivateIpAddress + "; " 
+  }
+}
+
+
+$parameters = @{}
+$parameters.Add(“vmAdminUsername”, $vmAdminUserName)
+$parameters.Add(“vmAdminPassword”, $vmAdminPassword)
+$parameters.Add(“vmSubnetId”, $vmSubnetIdHub )
+$parameters.Add(“vmStaticIP”, $vmStaticIPHub )
+$parameters.Add("vmName", $vmNameHub)
+$parameters.Add("vmSize", $vmSizeHub)
+$parameters.Add("extensionScript",$extensionScriptHub)
+
+
+New-AzureRmResourceGroupDeployment -ResourceGroupName $rgName  -TemplateFile $templateUri -TemplateParameterObject $parameters -Debug  -Verbose
